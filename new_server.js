f29e3052a76d196cbb6d0b3feeda308adfe668a9
fastoch <fastoch@ik.me>
2023-11-20 21:28:18 +0100

    chapter 6 - 132min

--- new_server.js
+++ new_server.js
@@ -1,4 +1,14 @@
-// old_server = version with Node.js only
-// new_server = version with Express.js 
-
+const express = require('express');
+const app = express(); // This is our web server
 const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running and listening on port ${PORT}`));
